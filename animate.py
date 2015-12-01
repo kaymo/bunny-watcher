847d83d6ec953f00490f1830c1a8c60fc5cb5a0d
Kim Mroz <kim.mroz17@gmail.com>
2015-12-01 12:49:30 +0000

    Make a gif from the day's captures (run at midnight with crontab)

---
+++ animate.py
@@ -0,0 +1,11 @@
+import os
+
+CAPTURES_DIR = "./static/captures"
+
+try:
+
+    # Create .gif
+    os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -pix_fmt rgb8 -ignore_loop 0 -s 576x384 {}/animated.gif".format(CAPTURES_DIR, CAPTURES_DIR) )
+
+except KeyboardInterrupt:
+    print "Quitting ..."
