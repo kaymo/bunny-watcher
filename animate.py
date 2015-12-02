2bb5ba3eaf0397bfbc6d7eb46f72b842be3de2a8
Kim Mroz <kim.mroz17@gmail.com>
2015-12-02 11:17:25 +0000

    Create and upload video to YouTube

--- animate.py
+++ animate.py
@@ -1,11 +1,24 @@
-import os
+import os, datetime
 
-CAPTURES_DIR = "/home/pi/bunny-watcher/static/captures"
+BUNNY_WATCHER_DIR = "/home/pi/bunny-watcher"
+CAPTURES_DIR = BUNNY_WATCHER_DIR + "/static/captures"
 
 try:
 
-    # Create .gif
+    # Create .gif for Bunny Watcher
     os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -pix_fmt rgb8 -ignore_loop 0 -s 576x384 {}/animated.gif".format(CAPTURES_DIR, CAPTURES_DIR) )
+
+    # Create .mp4 for YouTube
+    os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -vcodec mpeg4 -pix_fmt yuv420p {}/animated.mp4".format(CAPTURES_DIR, CAPTURES_DIR) )
+
+    # YouTube details
+    filename = "animated.mp4"
+    desc     = "Bunny Watcher"
+    category = "15"   # Pets & Animals YouTube category ID
+    title    = (datetime.date.today() - datetime.timedelta(1)).strftime('%A%e %B %Y') # Yesterday's date
+
+    # Upload to YouTube
+    os.system("python {}/upload_video.py --noauth_local_webserver --file='{}/{}' --title='{}' --description='{}' --category={}".format(BUNNY_WATCHER_DIR, CAPTURES_DIR, filename, title, desc, category))
 
 except KeyboardInterrupt:
     print "Quitting ..."
