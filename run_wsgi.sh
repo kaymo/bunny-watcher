8be904ea0fab6055dfcd8c17f249c0c91bb798b7
Kim Mroz <kim.mroz17@gmail.com>
2015-12-03 17:54:27 +0000

    AVJ: adding nginx confs

--- run_wsgi.sh
+++ run_wsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 env PATH=/home/pi/bunny-watcher/bunny-watcherenv/bin
-chdir /home/pi/bunny-watcher
-exec uwsgi --ini bunny-watcher.ini
+cd /home/pi/bunny-watcher
+exec uwsgi --ini bunnies.ini
