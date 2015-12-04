1cb4409b3ba4cdc557145e58a4e291b88cc747ed
Kim Mroz <kim.mroz17@gmail.com>
2015-12-04 10:22:30 +0000

    Increase .mp4 quality and update gitignore

--- animate.py
+++ animate.py
@@ -9,7 +9,7 @@ try:
     os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -pix_fmt rgb8 -ignore_loop 0 -s 576x384 {}/animated.gif".format(CAPTURES_DIR, CAPTURES_DIR) )
 
     # Create .mp4 for YouTube
-    os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -vcodec mpeg4 -pix_fmt yuv420p {}/animated.mp4".format(CAPTURES_DIR, CAPTURES_DIR) )
+    os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -vcodec mpeg4 -q:v 10 {}/animated.mp4".format(CAPTURES_DIR, CAPTURES_DIR) )
 
     # YouTube details
     filename = "animated.mp4"
