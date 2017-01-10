506a5ad735cc1c0fceb16f5e949ddb3206eeb431
Andrew V. Jones <avj@vectorcast.com>
2017-01-10 08:47:04 +0000

    AVJ: no longer making the gif + saving the mp4

--- animate.py
+++ animate.py
@@ -1,15 +1,28 @@
-import os, datetime
+#!/usr/bin/env python
+
+import os
+import datetime
+import shutil
 
 BUNNY_WATCHER_DIR = "/home/pi/bunny-watcher"
 CAPTURES_DIR = BUNNY_WATCHER_DIR + "/static/captures"
+VIDEOS_DIR = BUNNY_WATCHER_DIR + "/static/videos"
+
+if not os.path.exists(VIDEOS_DIR):
+    os.makedirs(VIDEOS_DIR)
 
 try:
 
     # Create .gif for Bunny Watcher
-    os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -pix_fmt rgb8 -ignore_loop 0 -s 576x384 {}/animated.gif".format(CAPTURES_DIR, CAPTURES_DIR) )
+    # os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -pix_fmt rgb8 -ignore_loop 0 -s 576x384 {}/animated.gif".format(CAPTURES_DIR, CAPTURES_DIR) )
 
     # Create .mp4 for YouTube
     os.system("ffmpeg -y -framerate 10 -pattern_type glob -i '{}/2*.jpg' -vcodec mpeg4 -q:v 10 {}/animated.mp4".format(CAPTURES_DIR, CAPTURES_DIR) )
+
+    # Copy the mp4 to the back-up location
+    curr_time = datetime.datetime.now().isoformat().replace(".", "_").replace(":", "_")
+    curr_mp4_path = os.path.join(VIDEOS_DIR, curr_time, ".mp4")
+    shutil.copyfile(curr_mp4_path, CURRENT_CAPTURE)
 
     # YouTube details
     filename = "animated.mp4"
