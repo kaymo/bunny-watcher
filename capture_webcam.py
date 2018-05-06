6627bdfabb33391fad48f2ef80468c5dd45d328f
Andrew V. Jones <andrewvaughanj@gmail.com>
2018-05-06 21:27:40 +0100

    AVJ: refactoring out camera logic into separate class

---
+++ capture_webcam.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+# Dependencies: OpenCV
+import cv2
+import time
+import datetime
+import shutil
+import os
+import cv2.cv as cv
+import numpy
+
+if 'profile' not in globals():
+    def profile(func):
+        return func
+
+
+class CVWebcam(object):
+
+    def __init__(self, capture_location):
+
+        self.CAMERA_PORT = 0
+        self.CAPTURES_DIR = capture_location
+        self.CURRENT_CAPTURE = os.path.join(self.CAPTURES_DIR, "current.jpg")
+        self.INTERVAL = 60
+        self.JPEG_QUALITY = 85
+
+        self.FRAME_HEIGHT = 1536
+        self.FRAME_WIDTH = 2304
+        self.AUTO_BRIGHTNESS = 0.42
+        self.AUTO_SATURATION = 0.46
+        self.AUTO_CONTRAST = 0.46
+
+        self.RGB_DARK_THRESHOLD = 40
+
+    def get_image(self):
+        """
+        Captures a single image from the camera and returns it in PIL format
+        """
+
+        def get_property(property, property_id):
+            # print "{}: {}".format(property, camera.get(property_id))
+            pass
+
+        # Open the camera device and create OpenCV object
+        camera = cv2.VideoCapture(self.CAMERA_PORT)
+
+        if False:
+            # Get the initial amera properties
+            get_property("width",      cv.CV_CAP_PROP_FRAME_WIDTH)
+            get_property("height",     cv.CV_CAP_PROP_FRAME_HEIGHT)
+            get_property("brightness", cv.CV_CAP_PROP_BRIGHTNESS)
+            get_property("contrast",   cv.CV_CAP_PROP_CONTRAST)
+            get_property("saturation", cv.CV_CAP_PROP_SATURATION)
+
+        # Ensure that sensible values are set
+        camera.set(cv.CV_CAP_PROP_BRIGHTNESS,   self.AUTO_BRIGHTNESS)
+        camera.set(cv.CV_CAP_PROP_CONTRAST,     self.AUTO_CONTRAST)
+        camera.set(cv.CV_CAP_PROP_SATURATION,   self.AUTO_SATURATION)
+        camera.set(cv.CV_CAP_PROP_FRAME_WIDTH,  self.FRAME_WIDTH)
+        camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, self.FRAME_HEIGHT)
+
+        if False:
+            get_property("brightness", cv.CV_CAP_PROP_BRIGHTNESS)
+            get_property("contrast",   cv.CV_CAP_PROP_CONTRAST)
+            get_property("saturation", cv.CV_CAP_PROP_SATURATION)
+            get_property("width",      cv.CV_CAP_PROP_FRAME_WIDTH)
+            get_property("height",     cv.CV_CAP_PROP_FRAME_HEIGHT)
+
+        retval, image = camera.read()
+
+        ret_val = False
+
+        # Discard any frames that are too dark
+        if retval and len(image) and len(image[0]) and len(image[0][0]):
+
+            # Keep image if the first pixel's avergae is greater than the threshold
+            if sum(image[0][0]) / len(image[0][0]) > self.RGB_DARK_THRESHOLD:
+                ret_val = True
+
+            #
+            # Keep image if the average pixel value is greater than a threshold
+            #
+            # https://stackoverflow.com/questions/43111029/how-to-find-the-average-colour-of-an-image-in-python-with-opencv
+            #
+            avg_color_per_row = numpy.average(image, axis=0)
+            avg_color_img = numpy.average(avg_color_per_row, axis=0)
+            avg_pixel = numpy.average(avg_color_img, axis=0)
+
+            if avg_pixel > self.RGB_DARK_THRESHOLD:
+                ret_val = True
+
+        del(camera)
+        return ret_val, image
+
+    def capture(self, time_now):
+        """
+        Capture image and output to filename based on time_now param
+        """
+        time_str = time_now.strftime("%Y-%m-%d %H:%M:%S")
+        [result, camera_capture] = self.get_image()
+
+        if result:
+
+            # Use the current time as the filename
+            curr_time = time_now.isoformat().replace(".", "_").replace(":", "_")
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.putText(camera_capture, time_str, (20, 70),
+                        font, 2.5, (255, 255, 255), 4)
+
+            # Store the image in the history and copy over the 'current' view
+
+            output_img = os.path.join(
+                self.CAPTURES_DIR, "{:s}.jpg".format(curr_time))
+            cv2.imwrite(output_img, camera_capture,
+                        [cv2.cv.CV_IMWRITE_JPEG_QUALITY, self.JPEG_QUALITY])
+            shutil.copyfile(
+                output_img, self.CURRENT_CAPTURE)
+        else:
+            curr_time = time_now.isoformat().replace(".", "_").replace(":", "_")
+            print "Failed: " + curr_time
+
+
+if __name__ == "__main__":
+    time_now = datetime.datetime.now()
+
+    webcam = CVWebcam("/tmp")
+    webcam.capture(time_now)
+
+# EOF
--- sense.py
+++ sense.py
@@ -9,147 +9,53 @@ import os
 import cv2.cv as cv
 import numpy
 
-if 'profile' not in globals():
-    def profile(func):
-        return func
-
-CAMERA_PORT = 0
-CAPTURES_DIR = "static/captures/"
-CURRENT_CAPTURE = CAPTURES_DIR + "current.jpg"
-INTERVAL = 60
-JPEG_QUALITY = 85
-
-FRAME_HEIGHT = 1536
-FRAME_WIDTH = 2304
-AUTO_BRIGHTNESS = 0.42
-AUTO_SATURATION = 0.46
-AUTO_CONTRAST = 0.46
-
-RGB_DARK_THRESHOLD = 40
-
-# Captures a single image from the camera and returns it in PIL format
+from capture_webcam import CVWebcam
 
 
-@profile
-def get_image():
+class BunnySense(object):
 
-    def get_property(property, property_id):
-        # print "{}: {}".format(property, camera.get(property_id))
-        pass
+    def __init__(self, output_dir, interval):
+        self.CAPTURES_DIR = output_dir
+        self.webcam = CVWebcam(output_dir)
+        self.INTERVAL = interval
 
-    # Open the camera device and create OpenCV object
-    camera = cv2.VideoCapture(CAMERA_PORT)
+    def main(self):
 
-    if False:
-        # Get the initial amera properties
-        get_property("width",      cv.CV_CAP_PROP_FRAME_WIDTH)
-        get_property("height",     cv.CV_CAP_PROP_FRAME_HEIGHT)
-        get_property("brightness", cv.CV_CAP_PROP_BRIGHTNESS)
-        get_property("contrast",   cv.CV_CAP_PROP_CONTRAST)
-        get_property("saturation", cv.CV_CAP_PROP_SATURATION)
+        try:
+            while 1:
+                print "LOOP"
+                time_now = datetime.datetime.now()
 
-    # Ensure that sensible values are set
-    camera.set(cv.CV_CAP_PROP_BRIGHTNESS,   AUTO_BRIGHTNESS)
-    camera.set(cv.CV_CAP_PROP_CONTRAST,     AUTO_CONTRAST)
-    camera.set(cv.CV_CAP_PROP_SATURATION,   AUTO_SATURATION)
-    camera.set(cv.CV_CAP_PROP_FRAME_WIDTH,  FRAME_WIDTH)
-    camera.set(cv.CV_CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT)
+                self.webcam.capture(time_now)
 
-    if False:
-        get_property("brightness", cv.CV_CAP_PROP_BRIGHTNESS)
-        get_property("contrast",   cv.CV_CAP_PROP_CONTRAST)
-        get_property("saturation", cv.CV_CAP_PROP_SATURATION)
-        get_property("width",      cv.CV_CAP_PROP_FRAME_WIDTH)
-        get_property("height",     cv.CV_CAP_PROP_FRAME_HEIGHT)
+                # Delete if any images older than a day
+                for fn in sorted(os.listdir(self.CAPTURES_DIR)):
+                    fp = os.path.join(self.CAPTURES_DIR, fn)
+                    if os.stat(fp).st_mtime < time.time() - 86400:
+                        if os.path.isfile(fp):
+                            os.remove(fp)
 
-    retval, image = camera.read()
+                    # The files are ordered by filename (by age) so stop if a young
+                    # file is found
+                    else:
+                        break
 
-    ret_val = False
+                end = datetime.datetime.now()
+                elapsed = end - time_now
 
-    # Discard any frames that are too dark
-    if retval and len(image) and len(image[0]) and len(image[0][0]):
+                interval = datetime.timedelta(seconds=self.INTERVAL)
 
-        # Keep image if the first pixel's avergae is greater than the threshold
-        if sum(image[0][0]) / len(image[0][0]) > RGB_DARK_THRESHOLD:
-            ret_val = True
+                sleep_cnt = interval - elapsed
 
-        #
-        # Keep image if the average pixel value is greater than a threshold
-        #
-        # https://stackoverflow.com/questions/43111029/how-to-find-the-average-colour-of-an-image-in-python-with-opencv
-        #
-        avg_color_per_row = numpy.average(image, axis=0)
-        avg_color_img = numpy.average(avg_color_per_row, axis=0)
-        avg_pixel = numpy.average(avg_color_img, axis=0)
+                time.sleep(sleep_cnt.total_seconds())
 
-        if avg_pixel > RGB_DARK_THRESHOLD:
-            ret_val = True
-
-    del(camera)
-    return ret_val, image
-
-# Get and print an OpenCV property
+        except KeyboardInterrupt:
+            print "\nQuitting ..."
 
 
-# Capture a still at regular intervals
-print
-print("Capturing images ...")
-
-
-@profile
-def main():
-
-    try:
-        while 1:
-            print "LOOP"
-            start = datetime.datetime.now()
-            # Grab the image
-            time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            [result, camera_capture] = get_image()
-
-            if result:
-
-                # Use the current time as the filename
-                curr_time = datetime.datetime.now().isoformat().replace(".", "_").replace(":", "_")
-
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                cv2.putText(camera_capture, time_str, (20, 70),
-                            font, 2.5, (255, 255, 255), 4)
-
-                # Store the image in the history and copy over the 'current' view
-                cv2.imwrite(CAPTURES_DIR + curr_time + ".jpg", camera_capture,
-                            [cv2.cv.CV_IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
-                shutil.copyfile(
-                    CAPTURES_DIR + curr_time + ".jpg", CURRENT_CAPTURE)
-            else:
-                curr_time = datetime.datetime.now().isoformat().replace(".", "_").replace(":", "_")
-                print "Failed: " + curr_time
-
-            # Delete if any images older than a day
-            for fn in sorted(os.listdir(CAPTURES_DIR)):
-                fp = os.path.join(CAPTURES_DIR, fn)
-                if os.stat(fp).st_mtime < time.time() - 86400:
-                    if os.path.isfile(fp):
-                        os.remove(fp)
-
-                # The files are ordered by filename (by age) so stop if a young
-                # file is found
-                else:
-                    break
-
-            end = datetime.datetime.now()
-            elapsed = end - start
-
-            interval = datetime.timedelta(seconds=INTERVAL)
-
-            sleep_cnt = interval - elapsed
-
-            time.sleep(sleep_cnt.total_seconds())
-
-    except KeyboardInterrupt:
-        print "\nQuitting ..."
-
-
-main()
+if __name__ == "__main__":
+    capture_dir = "/tmp"
+    bunny_sense = BunnySense(capture_dir, 10)
+    bunny_sense.main()
 
 # EOF
