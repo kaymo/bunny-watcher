a0496ee8665ed04e2c546ebbf7b9e085bd51a142
Andrew V. Jones <andrewvaughanj@gmail.com>
2018-05-06 21:05:55 +0100

    AVJ: camera init (e.g., disable LED)

--- run_sense.sh
+++ run_sense.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo uvcdynctrl -s 'LED1 Mode' 0
+sudo uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml |& cat > /dev/null
+sudo uvcdynctrl --set='Focus, Auto' 1
+sudo uvcdynctrl -s 'LED1 Mode' 0 |& cat > /dev/null
 
 cd /home/pi/bunny-watcher
 # kernprof --line-by-line ./sense.py
