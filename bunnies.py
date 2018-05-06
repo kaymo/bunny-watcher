c666d3e6585156810be5f361484aebc4a8bdded2
Andrew V. Jones <andrewvaughanj@gmail.com>
2018-05-06 22:23:36 +0100

    Changing code to hard-display webcam

--- bunnies.py
+++ bunnies.py
@@ -17,17 +17,26 @@ application.config['DEBUG'] = False
 def show_capture():
 
     # Get a list of the captures that doesn't include the current captures
-    images = sorted(os.listdir('static/captures/'), reverse=True)
+    images = sorted(os.listdir('static/captures/webcam/'), reverse=True)
     # remove "current" and "animated"
-    images.remove("current.jpg")
-    images.remove("animated.mp4")
+    try:
+        images.remove("current.jpg")
+    except:
+        pass
+
+    try:
+        images.remove("animated.mp4")
+    except:
+        pass
     # and now remove whatever is currently showing!
     current = images[0]
     images = images[1:]
 
     videos = sorted(os.listdir('static/videos/'), reverse=True)
 
-    return render_template('main.html', current=current, images=images, videos=videos)
+    return render_template(
+        'main.html', current=current, images=images, videos=videos)
+
 
 # Start the app and make available to all
 if __name__ == "__main__":
