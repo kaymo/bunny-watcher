2b04b2918fbcb3e3518a3d7336ff3fb009f34d69
Andrew V. Jones <andrewvaughanj@gmail.com>
2018-05-07 22:42:44 +0100

    Restarting if no sequential images from thermcam

--- run_sense.sh
+++ run_sense.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-sudo uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml |& cat > /dev/null
-sudo uvcdynctrl --set='Focus, Auto' 0
+# 
+# sudo uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml |& cat > /dev/null
+# sudo uvcdynctrl --set='Focus, Auto' 0
 sudo uvcdynctrl -s 'LED1 Mode' 0
-sudo v4l2-ctl -d /dev/video0 -c led1_mode=0
-sudo v4l2-ctl -d /dev/video0 -c focus_auto=0
-sudo v4l2-ctl -d /dev/video0 -c focus_absolute=0
+# sudo v4l2-ctl -d /dev/video0 -c led1_mode=0
+# sudo v4l2-ctl -d /dev/video0 -c focus_auto=0
+# sudo v4l2-ctl -d /dev/video0 -c focus_absolute=0
+# 
 
 cd /home/pi/bunny-watcher
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 # kernprof --line-by-line ./sense.py
-python ./sense.py
+while true; do python ./sense.py; sleep 2; done
+
