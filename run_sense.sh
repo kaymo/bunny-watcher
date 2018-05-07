9a396247ec5e0a77065516a0cee30cd898b4cab3
Andrew V. Jones <andrewvaughanj@gmail.com>
2018-05-07 21:53:50 +0100

    Sense now tries to set focus to avoid blurry images

--- run_sense.sh
+++ run_sense.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 sudo uvcdynctrl -i /usr/share/uvcdynctrl/data/046d/logitech.xml |& cat > /dev/null
-sudo uvcdynctrl --set='Focus, Auto' 1
-sudo uvcdynctrl -s 'LED1 Mode' 0 |& cat > /dev/null
+sudo uvcdynctrl --set='Focus, Auto' 0
+sudo uvcdynctrl -s 'LED1 Mode' 0
+sudo v4l2-ctl -d /dev/video0 -c led1_mode=0
+sudo v4l2-ctl -d /dev/video0 -c focus_auto=0
+sudo v4l2-ctl -d /dev/video0 -c focus_absolute=0
 
 cd /home/pi/bunny-watcher
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
