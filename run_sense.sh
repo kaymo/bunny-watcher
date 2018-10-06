9bbe3b178fa0587537c8c49c2adc637699c01613
andrewvaughanj@gmail.com <Andrew V. Jones>
2018-10-06 19:42:54 +0100

    Sense runs with a high niceness

--- run_sense.sh
+++ run_sense.sh
@@ -5,5 +5,5 @@
 # cd /home/pi/bunny-watcher
 export LD_LIBRARY_PATH=$(pwd):${LD_LIBRARY_PATH}
 # kernprof --line-by-line ./sense.py
-while true; do echo "STARTING"; python -u ./sense.py; sleep 2; done
+while true; do echo "STARTING"; nice -n -20 python -u ./sense.py; sleep 2; done
 
